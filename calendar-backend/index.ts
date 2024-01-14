2b6ea19691d0174f23332b85aa07c611cd3d3da9
dealwith <gleb.krishin@icloud.com>
2024-01-14 18:55:43 +0100

    Initializing backend to handle google auth

--- calendar-backend/index.ts
+++ calendar-backend/index.ts
@@ -1,15 +1,66 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import { google }  from 'googleapis';
+import cors from 'cors';
 
 dotenv.config();
 
+const oauth2Client = new google.auth.OAuth2(
+	process.env.CLIENT_ID,
+	process.env.CLIENT_SECRET,
+	process.env.REDIRECT_URI
+);
+
+const scopes = [
+	'https://www.googleapis.com/auth/calendar.events'
+]
+
+const authorizationUrl = oauth2Client.generateAuthUrl({
+	access_type: 'offline',
+	scope: scopes,
+	include_granted_scopes: true
+});
+
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Welcome to Express & TypeScript Server');
 });
 
+app.get('/api/auth/google', (req: Request, res: Response) => {
+	res.redirect(authorizationUrl);
+})
+
+app.get('/api/auth/google/callback', (req: Request, res: Response) => {
+	const { code } = req.query;
+
+	console.log(code)
+	if (code) {
+		oauth2Client.getToken(code.toString(), (err, token) => {
+			if (err) {
+				console.log('Error while trying to retrieve access token', err);
+				return res.status(500).send('Error while trying to retrieve access token');
+			}
+
+			if (token) {
+				oauth2Client.setCredentials(token);
+				return res.status(200).send(token);
+			}
+		})
+	}
+
+	return res.status(400).send('Bad Request');
+})
+
+app.get('api/calendar/events', async (req: Request, res: Response) => {
+
+});
+
 app.listen(port, () => {
 	console.log(`Server is Fire at http://localhost:${port}`);
 });
+
+
--- calendar-client/src/App.tsx
+++ calendar-client/src/App.tsx
@@ -1,13 +1,16 @@
 import { Box, Button, Container, Heading } from '@radix-ui/themes'
 import './index.css'
+import axios from 'axios'
 
 
 function App() {
+  const authorizeGoogle = () => axios.get('http://localhost:8000/api/auth/google')
+
   return (
     <Box width='100%' height='100%'>
       <Container size='4'>
         <Heading align='center' size='9'>Google Calendar</Heading>
-        <Button mt='9' size='3'>Connect calendar</Button>
+        <Button onClick={authorizeGoogle} mt='9' size='3'>Connect calendar</Button>
       </Container>
     </Box>
   )
