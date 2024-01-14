ba6dbbd942fe2f9f9bb548e5aabe03374d93c7f7
dealwith <gleb.krishin@icloud.com>
2024-01-14 23:40:37 +0100

    Refactor calendar client code and add GoogleEvents type

--- calendar-client/src/App.tsx
+++ calendar-client/src/App.tsx
@@ -2,48 +2,48 @@ import { Box, Button, Container, Heading } from '@radix-ui/themes'
 import axios from 'axios'
 import './index.css'
 import { useState, useEffect } from 'react';
+import { GoogleEvents } from './Event';
 
 
 function App() {
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState<GoogleEvents>([]);
 
   useEffect(() => {
-    // Check if the URL has a code parameter
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
-
-    if (code) {
-      // Exchange the code for tokens
-      axios.get(`http://localhost:8000/api/exchange-code?code=${code}`)
-        .then(res => {
-          fetchEvents();
-        }).catch(err => console.error(err));
-    }
+    fetchEvents()
   }, []);
 
   const fetchEvents = () => {
-    axios.get('http://localhost:8000/calendar/events')
+    return axios.get('api/calendar/events', {withCredentials: true})
       .then(response => {
         setEvents(response.data);
       })
       .catch(error => console.error(error));
   };
 
-  const handleAuth = () => {
-    window.location.href = 'http://localhost:8000/api/auth/google';
+  const connectCalendar = () => {
+    window.location.href = '/api/auth/google';
   };
 
-  console.log(events);
+  const disconnectCalendar = async () => {
+    const res = await axios.post('/api/calendar/disconnect')
+    if (res.status === 200) {
+      setEvents([]);
+    }
+  }
 
+  const isEvents = events.length > 0;
   return (
     <Box width='100%' height='100%'>
       <Container size='4'>
         <Heading align='center' size='9'>Google Calendar</Heading>
-        <Button onClick={handleAuth} mt='9' size='3'>Connect calendar</Button>
+        {isEvents
+          ? <Button color='crimson' onClick={disconnectCalendar} mt='9' size='3'>Disconnect calendar</Button>
+          : <Button onClick={connectCalendar} mt='9' size='3'>Connect calendar</Button>
+        }
         <ul>
-        {/* {events.map((event: { id: Key | null | undefined; summary: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; start: { dateTime: string | number | Date; }; }) => (
-          <li key={event.id}>{event.summary} - {new Date(event.start.dateTime).toLocaleString()}</li>
-        ))} */}
+          {events.map(event => (
+            <li key={event.id}>{event.summary} - {new Date(event.start.dateTime).toLocaleString()}</li>
+          ))}
       </ul>
       </Container>
     </Box>
