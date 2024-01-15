8b5fddd2d802cd2d5710253b483574e4d6bc39be
dealwith <gleb.krishin@icloud.com>
2024-01-15 15:32:28 +0100

    Fix private key formatting in calendar-backend

--- calendar-backend/index.ts
+++ calendar-backend/index.ts
@@ -12,7 +12,9 @@ admin.initializeApp({
   credential: admin.credential.cert({
 		projectId: process.env.SERVICE_ACCOUNT_PROJECT_ID,
 		clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-		privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY!.replaceAll("\\n", "\n")
+		privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY!.includes('-----BEGIN')
+      ? process.env.SERVICE_ACCOUNT_PRIVATE_KEY
+      : process.env.SERVICE_ACCOUNT_PRIVATE_KEY!.replace(/\\n/g, '\n')
 	}),
 });
 
